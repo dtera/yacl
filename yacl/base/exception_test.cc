eabb8d8d31032ae5e2603749b0fc86053eefdff3
dterazhao <dterazhao@tencent.com>
2023-07-11 12:05:08 +0800

    [feat]: add third party library for cmake project

--- yacl/base/exception_test.cc
+++ yacl/base/exception_test.cc
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#pragma ide diagnostic ignored "readability-function-cognitive-complexity"
 
 #include "yacl/base/exception.h"
 
