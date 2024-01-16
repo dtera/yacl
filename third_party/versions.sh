96146f592fac66c801c8697ede2209230c26f8d0
dterazhao <dterazhao@tencent.com>
2024-01-16 16:27:26 +0800

    [feat]: rsync cmake project from bazel's and fix some problems

--- third_party/versions.sh
+++ third_party/versions.sh
@@ -4,15 +4,15 @@ CD=$(cd "$(dirname "$0")" || exit && pwd)
 cd "$CD" || exit
 
 absl_ver=20230802.1
-fmt_ver=10.1.1
+fmt_ver=10.2.1
 msgpack_ver=6.1.0
 openssl_ver=3.0.12
-spdlog_ver=1.12.0
-libtommath_ver=8ce69f7b5e2f34620633f4fb5c231045a8dc2f54
-blake3_ver=1.4.1
+spdlog_ver=1.13.0
+libtommath_ver=8314bde5e5c8e5d9331460130a9d1066e324f091
+blake3_ver=1.5.0
 libsodium_ver=1.0.18
 curve25519_ver=2fe66b65ea1acb788024f40a3373b8b3e6f4bbb2
-mcl_ver=1.84.0
+mcl_ver=1.86.0
 leveldb_ver=1.23
 brpc_ver=1.7.0
 grpc_ver=1.54.0
